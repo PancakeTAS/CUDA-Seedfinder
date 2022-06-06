206266357d48c5c72a5ba59f13217e70fb83110a
MCPfannkuchenYT <gamesupport@mgnet.work>
2022-06-06 17:28:44 +0200

    Test: More precise search

--- src/searcher.cu
+++ src/searcher.cu
@@ -8,27 +8,31 @@ __global__ void startSearch(int64_t structureSeedOffset) {
     int64_t index = ((structureSeedOffset + threadIdx.x) + (((int64_t) blockIdx.x) * 256));
     int64_t structureSeed = (index << 16) + structureSeedOffset;
 
-    // Find the nether fortress structure on the seed
-    uint64_t xz = locate_structure(structureSeed,
-        // Position Seed Part
-        0 * A + 0 * B, 
-        // Spaced X and Z Region Coordinates
-        0 * 27L,
-        0 * 27L,
-        // Salt and Offset
-        23L, 30084232LL,
-        // Edge Case
-        3
-    );
+    for (size_t cx = 0; cx < 3; cx++) {
+        size_t cz = cx;
 
-    if (xz == 0xFFFFFFFFFFFFFFFF) // Return if the structure wasn't found
-        return; 
+        // Find the nether fortress structure on the seed
+        uint64_t xz = locate_structure(structureSeed,
+            // Position Seed Part
+            cx * A + cz * B, 
+            // Spaced X and Z Region Coordinates
+            cx * 27L,
+            cz * 27L,
+            // Salt and Offset
+            23L, 30084232LL,
+            // Edge Case
+            3
+        );
 
-    int32_t x = xz >> 32;
-    int32_t z = xz;
+        if (xz == 0xFFFFFFFFFFFFFFFF) // Return if the structure wasn't found
+            return; 
 
-    if (x > 1 || z > 1)
-        return;
+        int32_t x = xz >> 32;
+        int32_t z = xz;
+
+        if (x > cx*27L+1 || z > cz*27L+1)
+            return;
+    }
 
     // Find and check for the correct biome on the seed
     nether_noise noise;
