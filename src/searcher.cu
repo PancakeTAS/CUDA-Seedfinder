a0500f328cfe148ccf6997f80c54fb59a1b99916
MCPfannkuchenYT <gamesupport@mgnet.work>
2022-06-06 16:32:08 +0200

    Finish nether biome generation

--- src/searcher.cu
+++ src/searcher.cu
@@ -1,11 +1,11 @@
 #include "searcher.hu"
 
-#define A 341873128712L
-#define B 132897987541L
+#define A 341873128712LL
+#define B 132897987541LL
 
 __global__ void startSearch(int64_t structureSeedOffset) {
     // Figure out what seed to check
-    int64_t index = ((structureSeedOffset + threadIdx.x) + (((int64_t) blockIdx.x) * 1024));
+    int64_t index = ((structureSeedOffset + threadIdx.x) + (((int64_t) blockIdx.x) * 256));
     int64_t structureSeed = (index << 16) + structureSeedOffset;
 
     // Find the nether fortress structure on the seed
@@ -16,7 +16,7 @@ __global__ void startSearch(int64_t structureSeedOffset) {
         0 * 27L,
         0 * 27L,
         // Salt and Offset
-        23L, 30084232L,
+        23L, 30084232LL,
         // Edge Case
         3
     );
@@ -27,7 +27,7 @@ __global__ void startSearch(int64_t structureSeedOffset) {
     int32_t x = xz >> 32;
     int32_t z = xz;
 
-    if (x > 1 && z > 1)
+    if (x > 1 || z > 1)
         return;
 
     // Find and check for the correct biome on the seed
